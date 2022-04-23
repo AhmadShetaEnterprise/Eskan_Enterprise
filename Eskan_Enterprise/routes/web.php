29009cef2fbafb43dbc834b8f82e950fc8cb5b26
AhmadShetaEnterprise <ahmadsheta.enterprise@gmail.com>
2022-04-23 12:01:40 +0200

    try
    try

--- Eskan_Enterprise/routes/web.php
+++ Eskan_Enterprise/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(AdminController::class)->group(function () {
+    Route::get('/orders/{id}', 'show');
+    Route::post('/orders', 'store');
+});
